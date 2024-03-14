fffaa939a435b53e7ab543bc300eb8420c0b761c
PierreWoL <118140290+PierreWoL@users.noreply.github.com>
2024-03-14 17:38:54 +0000

    Delete SemProp directory

--- SemProp/server_config.py
+++
@@ -1,8 +0,0 @@
-
-# input parameters
-#path_model = "/Users/ra-mit/development/discovery_proto/models/tpch/"
-#separator = "|"
-path_model = "/Users/ra-mit/development/discovery_proto/models/massdata/"
-separator = ","
-#path_model = "/Users/ra-mit/development/discovery_proto/models/mitdwh/"
-#separator = ","
